3aa8c00d9d464ab5c0733bbdfdc0b600cc6b2110
Bryan O <bocampo91@yahoo.com>
2024-04-08 02:08:42 -0400

    added html code in order to have modal load on the webpage

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,7 +2,7 @@
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 const createToDo = document.querySelector('#btn btn-success');
-const modelEntry = document.getElementById('to-do');
+const modelEntry = document.getElementById('modalEntry');
 
 //let task = '';
 
@@ -69,5 +69,5 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-
+    renderTaskList();
 });
